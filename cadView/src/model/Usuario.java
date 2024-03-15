c79b4b3e59c7c66f1a522a77b64ab06c1c186778
kellycmds239 <159198964+kellycmds239@users.noreply.github.com>
2024-03-14 21:58:57 -0300

    Add files via upload

---
+++ cadView/src/model/Usuario.java
@@ -0,0 +1,51 @@
+package model;
+
+
+public class Usuario {
+
+    private String nome;
+    private String email;
+    private String celular;
+    private String cpf;
+    private int idNivel;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdNivel() {
+        return idNivel;
+    }
+
+    public void setIdNivel(int idNivel) {
+        this.idNivel = idNivel;
+    }
+}
